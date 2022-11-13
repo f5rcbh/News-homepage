94b1e9f4b6fe60a608a6ce01f8ea4b09c1073322
khalid <f6807@hotmail.com>
2022-11-13 06:55:05 +0300

    finish navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Nav from './component/Nav';
+function App() {
+  return (
+    <div className='Card'>
+
+      <Nav/>
+    </div>
+    
+  )
+}
+
+export default App;
---
+++ src/component/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Nav = () => {
+  return ( 
+    
+    <nav className='nav'>
+        <ol className='nav-ol'>
+            <li className='nav-li'><a href=""className='nav-a'>Home</a></li>
+            <li className='nav-li'><a href=""className='nav-a'>New</a></li>
+            <li className='nav-li'><a href=""className='nav-a'>Popular</a></li>
+            <li className='nav-li'><a href=""className='nav-a'>Trending</a></li>
+            <li className='nav-li'><a href=""className='nav-a'>Categories</a></li>
+        </ol>
+    </nav>
+    
+  )
+}
+export default Nav
